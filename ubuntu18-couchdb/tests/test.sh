bb2d6bfcb94574ce92495bcb417c7eb9390b3844
Mercogliano Isidoro <mercogliano.isidoro@gmail.com>
2020-05-05 15:17:57 +0200

    correction for test file

--- ubuntu18-couchdb/tests/test.sh
+++ ubuntu18-couchdb/tests/test.sh
@@ -4,7 +4,7 @@ set -e
 
 
 #test with dgoss
-dgoss run -d  -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin_pass -e ROOT_PASSWORD=root_pass mercisi/ubuntu18-supervisord:couchdb
+dgoss run -d  -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin_pass -e ROOT_PASSWORD=root_pass mercisi/ubuntu18:couchdb
 
 
 
